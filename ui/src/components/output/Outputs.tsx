3112cddc16c76482a1ea4f080e1d19f65fc10697
Uzondu Enudeme <uzondu@orijtech.com>
2022-12-13 22:51:10 +0100

    ui: implement proto definitions in form UI with dummy output updates #1

--- ui/src/components/output/Outputs.tsx
+++ ui/src/components/output/Outputs.tsx
@@ -1,13 +1,20 @@
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
-export interface ResultProps {
-    //
+import { V1RunLoadtestResponse as Result } from 'src/gen/LoadtestApi';
+
+interface OutputProps {
+    data: Result;
 };
 
-export default function Inputs(props: ResultProps) {
+export default function Inputs(props: OutputProps) {
     return (
         <Box>
-            Output
+            <Typography variant='caption'>
+                <pre style={{ whiteSpace: 'pre-line', wordWrap: 'break-word' }}>
+                    {JSON.stringify(props.data, null, 2)}
+                </pre>
+            </Typography>
         </Box>
     )
 }
