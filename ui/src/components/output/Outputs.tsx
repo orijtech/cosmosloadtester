7687140701194f45e9d94815d4971d9d1c318127
Uzondu Enudeme <uzondu@orijtech.com>
2022-12-20 21:08:49 +0100

    ui: add graphs showing qps and bytes sent per #14
    updates #14
    
    TODO(uzo) complete the flow using the protobuf definitions and server response

--- ui/src/components/output/Outputs.tsx
+++ ui/src/components/output/Outputs.tsx
@@ -1,9 +1,11 @@
+import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
 
-import {
-    RunLoadtestResponse as Result,
-} from 'src/gen/orijtech/cosmosloadtester/v1/loadtest_service_pb';
+import { RunLoadtestResponse as Result } from 'src/gen/orijtech/cosmosloadtester/v1/loadtest_service_pb';
+
+import { sampleOutput } from './data';
+import Graphs from './Graphs';
 
 interface OutputProps {
     data: Result.AsObject;
@@ -17,22 +19,25 @@ interface ResultBarProps {
 function ResultBar(props: ResultBarProps) {
     const { title, value } = props;
     return (
-        <span style={{ marginRight: 5 }}>
-            <Chip label={title} size='small' variant='outlined' color='info'  sx={{ borderRadius: 0 }} />
-            <Chip label={value} size='small' variant='filled' color='info'  sx={{ borderRadius: 0 }} />
+        <span style={{ marginRight: 8 }}>
+            <Chip label={<Typography variant='caption' fontWeight='bold'>{title}</Typography>} variant='outlined' color='info'  sx={{ borderTopRightRadius: 0, borderBottomRightRadius: 0, borderRight: 'none' }} />
+            <Chip label={value} variant='outlined' color='info'  sx={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }} />
         </span>
     );
 }
 
-export default function Inputs(props: OutputProps) {
+export default function Outputs(props: OutputProps) {
     const { data } = props;
     return (
-        <Box>
-            <ResultBar title='avg bytes per second' value={data.avgBytesPerSecond} />
-            <ResultBar title='avg tx per second' value={data.avgTxsPerSecond} />
-            <ResultBar title='total bytes' value={data.totalBytes} />
-            <ResultBar title='total time' value={`${data.totalTime?.seconds || 0} seconds`} />
-            <ResultBar title='total tx' value={data.totalTxs} />
-        </Box>
+        <>
+            <Box>
+                <ResultBar title='avg bytes per second' value={data.avgBytesPerSecond} />
+                <ResultBar title='avg tx per second' value={data.avgTxsPerSecond} />
+                <ResultBar title='total bytes' value={data.totalBytes} />
+                <ResultBar title='total time' value={`${data.totalTime?.seconds || 0} seconds`} />
+                <ResultBar title='total tx' value={data.totalTxs} />
+            </Box>
+            <Graphs data={sampleOutput} />
+        </>
     );
 }
